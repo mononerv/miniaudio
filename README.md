8819ebb06bd9a3cd01a4e52c040fdf4c86aa0d04
mononerv <pratchaya.k.git@gmail.com>
2022-05-16 09:01:36 +0200

    v0.11.9

---
+++ README.md
@@ -0,0 +1,4 @@
+# miniaudio library
+
+A [miniaudio](https://github.com/mackron/miniaudio) fork with custom build for library linking usage.
+
---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -e
+
+in=miniaudio.c
+out="${in%.*}"
+
+bin=bin
+obj=obj
+
+c_version=-std=c18
+warnings="-Wall -Wextra"
+includes="-I."
+other="-O3"
+compile_flags="${c_version} ${warnings} ${includes} ${other}"
+
+mkdir -p $bin
+mkdir -p $obj
+
+cc $compile_flags -c $in -o $obj/${out}
+ar rcs $bin/lib${out}.a $obj/${out}
+
